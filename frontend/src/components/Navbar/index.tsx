4e3776ea2092535b081a9d707d9b46460d686b19
Darke Maciel <darke_maciel@hotmail.com>
2022-03-13 17:37:04 -0300

    Change /devsuperior on Navbar to /darkemaciel

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -11,7 +11,7 @@ function Navbar() {
                     <a href="https://github.com/darkemaciel/dsmovie">
                         <div className='dsmovie-contact-container'>
                             <GithubIcon />
-                            <p className='dsmovie-contact-link'>/devsuperior</p>
+                            <p className='dsmovie-contact-link'>/darkemaciel</p>
                         </div>
                     </a>
                 </div>
